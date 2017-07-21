ea610bcdf6e2abce91c7bdfc56b0be9978dc72cb
Connor Collins <connor.collins@gmail.com>
2017-07-21 15:35:35 -0400

    Made installable with Requirement already satisfied: pydogapi in /usr/local/lib/python2.7/site-packages

--- pydogapi/__init__.py
+++ pydogapi/__init__.py
@@ -1,5 +1,5 @@
 
-from __future__ import absolute_import, division, print_function, unicode_literals # For Python 2
+from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 if sys.version_info >= (3, 0):
     # For Python 3
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pydogapi',
+  packages = ['pydogapi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python wrapper for dog CEOs Dog API',
+  author = 'Connor Collins',
+  author_email = 'connor.collins@gmail.com',
+  url = 'https://github.com/c4collins/pydogapi', # use the URL to the github repo
+  download_url = 'https://github.com/c4collins/pydogapi/archive/0.1.tar.gz',
+  keywords = ['dogs'], # arbitrary keywords
+  classifiers = [],
+)
